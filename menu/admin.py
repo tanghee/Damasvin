7e5d306321d21776aad99cec677df76a8eb91751
s2018w37 <s2018w37@e-mirim.hs.kr>
2020-09-03 11:47:53 +0900

    menu/admin Drink, Coffee, Bubbletea

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from menu.models import Drink, Coffee, Bubbletea
+
+admin.site.register(Drink)
+admin.site.register(Coffee)
+admin.site.register(Bubbletea)
