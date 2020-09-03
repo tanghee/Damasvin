3f6debbada5862e83dde21066f8fcff8015a410a
s2018w37 <s2018w37@e-mirim.hs.kr>
2020-09-03 11:06:57 +0900

    menu/models Drink, Coffee, Bubbletea

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Drink(models.Model):
+    category = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    price = models.PositiveIntegerField()
+    image = models.ImageField(blank=True, upload_to='menu/%Y/%m/%d/')
+
+    def __str__(self):
+        return f'카테고리: {self.category}, 이름: {self.name}, 가격: {self.price}'
+
+class Coffee(Drink):
+    pass
+
+class Bubbletea(Drink):
+    pass
