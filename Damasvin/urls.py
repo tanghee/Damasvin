a72fc80e66cda2a311776cad7f372dbcf90a5f9e
s2018w37 <s2018w37@e-mirim.hs.kr>
2020-09-07 21:59:14 +0900

    urls, order/urls order:list

--- Damasvin/urls.py
+++ Damasvin/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('menu/', include('menu.urls')),
+    path('', include('order.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ order/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from order.views import OrderListView
+
+app_name = 'order'
+
+urlpatterns = [
+    path('', OrderListView.as_view(), name='list'),
+]
