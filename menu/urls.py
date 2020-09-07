2f2e63c084bc21d4fc20eeeee01ff571ab08d384
s2018w37 <s2018w37@e-mirim.hs.kr>
2020-09-07 14:51:08 +0900

    menu/urls add_coffee, add_bubbletea, update, delete

--- menu/urls.py
+++ menu/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from menu.views import DrinkListView
+from menu.views import DrinkListView, CoffeeCreateView, BubbleteaCreateView, DrinkUpdateView, DrinkDeleteView
 
 app_name = 'menu'
 
 urlpatterns = [
     path('', DrinkListView.as_view(), name='list'),
+    path('add_coffee/', CoffeeCreateView.as_view(), name='add_coffee'),
+    path('add_bubbletea/', BubbleteaCreateView.as_view(), name='add_bubbletea'),
+    path('update/<int:pk>/', DrinkUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', DrinkDeleteView.as_view(), name='delete'),
 ]
