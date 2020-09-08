553e7ae0e6572ce37b3e54c9a98f4fba9eda5be4
s2018w37 <s2018w37@e-mirim.hs.kr>
2020-09-08 12:53:04 +0900

    order/views menu_list in OrderListView, drink in OrderCreateView

--- order/views.py
+++ order/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView
 
+from menu.models import Drink
 from order.models import Order
 
 
 class OrderListView(ListView):
     model = Order
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)   # context 를 부모로부터 호출해서 먼저 생성
+        context['menu_list'] = Drink.objects.all()
+        return context
 
 
 class OrderCreateView(CreateView):
@@ -14,6 +20,11 @@ class OrderCreateView(CreateView):
     fields = '__all__'   # 모델이 갖고 있는 모든 속성을 다 넣기 위함
     template_name_suffix = '_create'
     success_url = reverse_lazy('order:list')
+
+    def get_initial(self):  # 초기화 해주는 함수. 넘겨주는 값을 받아와야하기 때문에 함수 사용.
+        initial = super().get_initial()
+        initial['drink'] = self.kwargs.get('menu_id')
+        return initial
 
 
 # CBV : 클래스를 기반으로 한 View, 상속받아서 금방 만들 수 있음
