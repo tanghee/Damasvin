aca5d8debbbdc2534e038297fb0c154c9fcc50b4
s2018w37 <s2018w37@e-mirim.hs.kr>
2020-09-08 14:38:55 +0900

    order/urls order:result

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from order import views
-from order.views import OrderListView, OrderCreateView, OrderUpdateView
+from order.views import OrderListView, OrderCreateView, OrderUpdateView, OrderResultView
 
 app_name = 'order'
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('add/<int:menu_id>/', OrderCreateView.as_view(), name='add'),
     path('update/<int:pk>/', OrderUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', views.order_delete, name='delete'),
+    path('result/', OrderResultView.as_view(), name='result'),
 ]
