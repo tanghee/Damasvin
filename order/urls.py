fcc2fa0c6173a8894e26dce43d08a55c052e6cf4
s2018w37 <s2018w37@e-mirim.hs.kr>
2020-09-08 10:33:45 +0900

    order/urls order:add, update, delete

--- order/urls.py
+++ order/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from order.views import OrderListView
+from order import views
+from order.views import OrderListView, OrderCreateView, OrderUpdateView
 
 app_name = 'order'
 
 urlpatterns = [
     path('', OrderListView.as_view(), name='list'),
+    path('add/', OrderCreateView.as_view(), name='add'),
+    path('update/<int:pk>/', OrderUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', views.order_delete, name='delete'),
 ]
