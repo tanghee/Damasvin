16dfa35f28012ce0eaca4030b40d69dc528b362a
s2018w37 <s2018w37@e-mirim.hs.kr>
2020-09-09 09:14:15 +0900

    order/views price, drink_price

--- order/views.py
+++ order/views.py
@@ -11,22 +11,29 @@ class OrderListView(ListView):
     model = Order
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)   # context 를 부모로부터 호출해서 먼저 생성
+        context = super().get_context_data(**kwargs)  # context 를 부모로부터 호출해서 먼저 생성
         context['menu_list'] = Drink.objects.all()
-        context['total_price'] = Order.objects.all().aggregate(total_price=Sum('price')).get('total_price', 0)    # aggregate 는 그룹으로 합산해서 계산해주는 함수
+        context['total_price'] = Order.objects.all().aggregate(total_price=Sum('price')).get('total_price',
+                                                                                             0)  # aggregate 는 그룹으로 합산해서 계산해주는 함수
         return context
 
 
 class OrderCreateView(CreateView):
     model = Order
-    fields = '__all__'   # 모델이 갖고 있는 모든 속성을 다 넣기 위함
+    fields = '__all__'  # 모델이 갖고 있는 모든 속성을 다 넣기 위함
     template_name_suffix = '_create'
     success_url = reverse_lazy('order:list')
 
     def get_initial(self):  # 초기화 해주는 함수. 넘겨주는 값을 받아와야하기 때문에 함수 사용.
         initial = super().get_initial()
         initial['drink'] = self.kwargs.get('menu_id')
+        initial['price'] = Drink.objects.get(id=self.kwargs.get('menu_id')).price
         return initial
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['drink_price'] = Drink.objects.get(id=self.kwargs['menu_id']).price
+        return context
 
 
 # CBV : 클래스를 기반으로 한 View, 상속받아서 금방 만들 수 있음
@@ -36,12 +43,17 @@ class OrderUpdateView(UpdateView):
     template_name_suffix = '_update'
     success_url = reverse_lazy('order:list')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['drink_price'] = Order.objects.get(id=self.kwargs['pk']).drink.price
+        return context
+
 
 # FBV
 # Template 작성하지 않고 함수 기반의 뷰를 사용. 바로 삭제를 위함.
 def order_delete(request, pk):
-    order = Order.objects.get(id=pk)     # DB 값을 꺼내오는 코드
-    order.delete()                       # DB 에서 값이 삭제됨.
+    order = Order.objects.get(id=pk)  # DB 값을 꺼내오는 코드
+    order.delete()  # DB 에서 값이 삭제됨.
     return redirect('order:list')
 
 
